5d38b1442ddad3740421e15d7cd8998c311c861f
glecca411 <glecca411@gmail.com>
2026-05-06 18:01:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 
 st.set_page_config(page_title="Asistente Académico", page_icon="🎓")
